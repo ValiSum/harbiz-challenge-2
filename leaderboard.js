c9d62115fb008da49f34d6cb03b3caf4d86d6e59
Vali Sumurduc <vali.sumurduc@gmail.com>
2024-06-01 20:57:55 +0200

    Adding collections to the database and get data

--- leaderboard.js
+++ leaderboard.js
@@ -4,15 +4,22 @@ import { Mongo } from "meteor/mongo";
 import { Session } from "meteor/session";
 import { Random } from "meteor/random";
 
-// Set up a collection to contain player information. On the server,
-// it is backed by a MongoDB collection named "players".
-
-const Players = new Mongo.Collection("players");
+const Scientists = new Mongo.Collection("scientists");
+const Athletes = new Mongo.Collection("athletes");
+const Actors = new Mongo.Collection("actors");
 
 if (Meteor.isClient) {
   Template.leaderboard.helpers({
-    players: function () {
-      return Players.find({}, { sort: { score: -1, name: 1 } });
+    characters: () => {
+      const category = Template.instance().data.category;
+      switch (category) {
+        case 'scientists':
+          return Scientists.find({}, { sort: { score: -1, name: 1 } });
+        case 'athletes':
+          return Athletes.find({}, { sort: { score: -1, name: 1 } });
+        case 'actors':
+          return Actors.find({}, { sort: { score: -1, name: 1 } });
+      }
     }
   });
 
@@ -30,7 +37,7 @@ if (Meteor.isClient) {
 
   Template.player.events({
     'click': function () {
-      const { category } = Template.parentData(1);
+      const category = Template.parentData(1).category;
 
       Session.set("selectedPlayer", this._id);
       Session.set("selectedName", this.name);
@@ -62,11 +69,33 @@ if (Meteor.isClient) {
 // On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    if (Players.find().count() === 0) {
+    if (Scientists.find().count() === 0) {
       const names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
                    "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
       names.forEach(function (name) {
-        Players.insert({
+        Scientists.insert({
+          name: name,
+          score: Math.floor(Random.fraction() * 10) * 5
+        });
+      });
+    }
+
+    if (Athletes.find().count() === 0) {
+      const names = ["Usain Bolt", "Michael Phelps", "Serena Williams",
+                   "Simone Biles", "Lebron James", "Tom Brady"];
+      names.forEach(function (name) {
+        Athletes.insert({
+          name: name,
+          score: Math.floor(Random.fraction() * 10) * 5
+        });
+      });
+    }
+
+    if (Actors.find().count() === 0) {
+      const names = ["Meryl Streep", "Tom Hanks", "Viola Davis",
+                   "Denzel Washington", "Cate Blanchett", "Leonardo DiCaprio"];
+      names.forEach(function (name) {
+        Actors.insert({
           name: name,
           score: Math.floor(Random.fraction() * 10) * 5
         });
