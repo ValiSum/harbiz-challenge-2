341e2d79a337950ede92733ecd314ce6c64712fa
Vali Sumurduc <vali.sumurduc@gmail.com>
2024-06-02 11:13:32 +0200

    Refactored code for better readability and maintainability

--- leaderboard.js
+++ leaderboard.js
@@ -8,45 +8,57 @@ const Scientists = new Mongo.Collection("scientists");
 const Athletes = new Mongo.Collection("athletes");
 const Actors = new Mongo.Collection("actors");
 
+const CATEGORIES = {
+  SCIENTISTS: 'scientists',
+  ATHLETES: 'athletes',
+  ACTORS: 'actors'
+};
+
+const POINTS = {
+  SCIENTISTS: 5,
+  ATHLETES: 10,
+  ACTORS: 15
+};
+
 if (Meteor.isClient) {
   Template.leaderboard.helpers({
     characters: () => {
       const category = Template.instance().data.category;
       switch (category) {
-        case 'scientists':
+        case CATEGORIES.SCIENTISTS:
           return Scientists.find({}, { sort: { score: -1, name: 1 } });
-        case 'athletes':
+        case CATEGORIES.ATHLETES:
           return Athletes.find({}, { sort: { score: -1, name: 1 } });
-        case 'actors':
+        case CATEGORIES.ACTORS:
           return Actors.find({}, { sort: { score: -1, name: 1 } });
       }
     }
   });
 
   Template.character.helpers({
-    selected: function () {
+    selected () {
       return Session.equals("selectedCharacter", this._id) ? "selected" : '';
     }
   });
 
   Template.character.events({
-    'click': function () {
+    'click' () {
       const category = Template.parentData(1).category;
       let characterType;
       let points;
 
       switch (category) {
-        case 'scientists':
+        case CATEGORIES.SCIENTISTS:
           characterType = 'scientist';
-          points = 5;
+          points = POINTS.SCIENTISTS;
           break;
-        case 'athletes':
+        case CATEGORIES.ATHLETES:
           characterType = 'athlete';
-          points = 10;
+          points = POINTS.ATHLETES;
           break;
-        case 'actors':
+        case CATEGORIES.ACTORS:
           characterType = 'actor';
-          points = 15;
+          points = POINTS.ACTORS;
           break;
       }
 
@@ -64,7 +76,7 @@ if (Meteor.isClient) {
   });
 
   Template.details.events({
-    'click .inc': () => {
+    'click .inc' () {
       const category = Session.get('selectedCategory');
       const selectedCharacterId = Session.get("selectedCharacter");
       const points = Session.get('selectedPoints');
@@ -87,37 +99,19 @@ if (Meteor.isClient) {
 // On server startup, create some characters if the database is empty.
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    if (Scientists.find().count() === 0) {
-      const names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
-                   "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
-      names.forEach(function (name) {
-        Scientists.insert({
-          name: name,
-          score: Math.floor(Random.fraction() * 10) * 5
+    const initCollection = (collection, names) => {
+      if (collection.find().count() === 0) {
+        names.forEach(name => {
+          collection.insert({
+            name,
+            score: Math.floor(Random.fraction() * 10) * 5
+          });
         });
-      });
-    }
+      }
+    };
 
-    if (Athletes.find().count() === 0) {
-      const names = ["Usain Bolt", "Michael Phelps", "Serena Williams",
-                   "Simone Biles", "Lebron James", "Tom Brady"];
-      names.forEach(function (name) {
-        Athletes.insert({
-          name: name,
-          score: Math.floor(Random.fraction() * 10) * 5
-        });
-      });
-    }
-
-    if (Actors.find().count() === 0) {
-      const names = ["Meryl Streep", "Tom Hanks", "Viola Davis",
-                   "Denzel Washington", "Cate Blanchett", "Leonardo DiCaprio"];
-      names.forEach(function (name) {
-        Actors.insert({
-          name: name,
-          score: Math.floor(Random.fraction() * 10) * 5
-        });
-      });
-    }
+    initCollection(Scientists, ["Ada Lovelace", "Grace Hopper", "Marie Curie", "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"]);
+    initCollection(Athletes, ["Usain Bolt", "Michael Phelps", "Serena Williams", "Simone Biles", "Lebron James", "Tom Brady"]);
+    initCollection(Actors, ["Meryl Streep", "Tom Hanks", "Viola Davis", "Denzel Washington", "Cate Blanchett", "Leonardo DiCaprio"]);
   });
 }
